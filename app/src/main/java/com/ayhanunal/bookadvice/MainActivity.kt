82d83ab4a6502c0d3a951c09ea2617f64fafbac2
ayhanunal <unalayhan@yahoo.com>
2021-05-16 14:04:06 +0300

    splash screen

--- app/src/main/java/com/ayhanunal/bookadvice/MainActivity.kt
+++ app/src/main/java/com/ayhanunal/bookadvice/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.ayhanunal.bookadvice
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        //splash screen
+        val intent = Intent(this@MainActivity, HomeActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
